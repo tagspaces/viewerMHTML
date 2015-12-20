c5b94952e4a760dd8690245cd51f7cf52c5e4b59
uggrock <ugg.rock@gmail.com>
2015-12-20 19:10:41 +0100

    js styling

--- viewer.js
+++ viewer.js
@@ -20,7 +20,7 @@ function setContent(content, done) {
     // making all links open in the user default browser
     $("#mhtmlViewer").find("a").bind('click', function(e) {
       e.preventDefault();
-      var msg = { command: "openLinkExternally", link : $(this).attr("href") };
+      var msg = {command: "openLinkExternally", link : $(this).attr("href")};
       window.parent.postMessage(JSON.stringify(msg), "*");
     }).css("cursor", "default");
 
